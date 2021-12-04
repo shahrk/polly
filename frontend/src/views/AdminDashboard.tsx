03a749a0e4f18424bd6100d8e07c4f48c059ac0e
Ishan Bhatt <ivbhatt@ncsu.edu>
2021-12-04 17:01:01 -0500

    Update AdminDashboard.tsx
    Adding borders to each of the sections to improve visibility

--- frontend/src/views/AdminDashboard.tsx
+++ frontend/src/views/AdminDashboard.tsx
@@ -64,13 +64,13 @@ export default function AdminDashboard({ history }: RouteComponentProps) {
                 'rgba(255, 205, 86, 0.7)',
                 'rgba(75, 192, 192, 0.7)',
               ],
-              // borderColor: [
-              //   'rgb(255, 99, 132)',
-              //   'rgb(255, 159, 64)',
-              //   'rgb(255, 205, 86)',
-              //   'rgb(75, 192, 192)',
-              // ],
-              // borderWidth: 1
+              borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+              ],
+              borderWidth: 1
             }
           ]
         }}
